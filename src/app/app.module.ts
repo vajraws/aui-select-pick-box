69e16f3cd100f4287f054057f1bb2fb29b74c33b
Sri M <srikanth.madasu@gmail.com>
2019-06-06 16:43:04 -0400

    fixed the missing component error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SelectBoxComponent, ListFilterPipe, SelectBoxModule } from 'projects/select-box/src/public_api';
+import { SelectBoxModule } from 'projects/select-box/src/public_api';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { TestFormControlComponent } from './comp/test-form-control/test-form-control.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestFormControlComponent,
   ],
   imports: [
     BrowserModule,
